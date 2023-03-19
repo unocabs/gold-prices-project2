808aad8fe9c5890f57e0f1412d5e6139af0b8e79
unocabs <rgiancabrera@gmail.com>
2023-03-20 07:45:52 +0800

    Real final

--- Gold Prices Chart.py
+++ Gold Prices Chart.py
@@ -1,5 +1,1 @@
-import webbrowser
-
-url = "https://public.tableau.com/app/profile/gian.cabrera/viz/GoldPrices_16792685986070/Sheet1?publish=yes"
-
-webbrowser.open(url)
+https://public.tableau.com/app/profile/gian.cabrera/viz/GoldPrices_16792685986070/Sheet1?publish=yes
